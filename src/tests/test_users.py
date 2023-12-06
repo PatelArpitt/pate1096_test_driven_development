9b99daf86441098e6c81cd2b079f88c8ee21ff00
PatelArpitt <patelarpit190@gmail.com>
2023-12-05 23:48:29 -0500

    initial commit

---
+++ src/tests/test_users.py
@@ -0,0 +1,61 @@
+# src/tests/test_users.py
+import json
+# from src import db
+from src.api.models import User
+
+def test_add_user(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'john',
+            'email': 'john@algonquincollege.com'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert 'john@algonquincollege.com was added!' in data['message']
+
+def test_single_user(test_app, test_database):
+    user = User(username='jeffrey', email='jeffrey@testdriven.io')
+    db.session.add(user)
+    db.session.commit()
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'jeffrey' in data['username']
+    assert 'jeffrey@testdriven.io' in data['email']
+
+def test_single_user(test_app, test_database, add_user):
+    user = add_user('jeffrey', 'jeffrey@testdriven.io')
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'jeffrey' in data['username']
+    assert 'jeffrey@testdriven.io' in data['email']
+
+def test_delete_user(test_app, test_database, add_user):
+    user = add_user('john', 'john@algonquincollege.com')
+    client = test_app.test_client()
+    resp = client.delete(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert f'{user.email} was removed!' in data['message']
+
+
+def test_all_users(test_app, test_database, add_user):
+    test_database.session.query(User).delete()  # new
+    add_user('john', ' john@algonquincollege.com')
+    add_user('fletcher', 'fletcher@notreal.com')
+    client = test_app.test_client()
+    resp = client.get('/users')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert len(data) == 2
+    assert 'john' in data[0]['username']
+    assert 'john@algonquincollege.com' in data[0]['email']
+    assert 'fletcher' in data[1]['username']
+    assert 'fletcher@notreal.com' in data[1]['email']
